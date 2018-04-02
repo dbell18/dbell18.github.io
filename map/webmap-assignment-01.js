b12aae1bec5fc14c3d1eb0a8133e72029495b9de
dbell18 <dbell18@lsu.edu>
2018-04-02 11:55:37 -0500

    Map 1

--- map/webmap-assignment-01.js
+++ map/webmap-assignment-01.js
@@ -1,15 +1,16 @@
 //let someName = L.map('your-map-div-id').setView([yourLat, yourLon], yourZoom)
-let mymap = L.map('mappicture').setView([51.505, -0.09], 13)
+let mymap = L.map('mappicture').setView([30.4583, -91.1403], 13)
 L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}').addTo(mymap)
-let marker = L.marker([51.5, -0.09]).addTo(mymap)
+let marker = L.marker([30.4117, -91.18425]).addTo(mymap)
 let polygon = L.polygon([
-  [51.509, -0.08],
-  [51.503, -0.06],
-  [51.51, -0.047]
+  [30.34227222, -91.22166667],
+  [30.32926944, -91.02805556],
+  [30.70777222, -90.8591667],
+  [30.69195556, -91.26277778]
 ]).addTo(mymap)
 
-marker.bindPopup('London Bridge Station')
-polygon.bindPopup('St. Katharine\'s and Wapping')
+marker.bindPopup('Louisiana State University Tiger Stadium')
+polygon.bindPopup('East Baton Rouge Parish')
 
 function logCurrentCoordinates (event) {
   console.log('You clicked the map at ' + event.latlng)
@@ -17,8 +18,8 @@ function logCurrentCoordinates (event) {
 mymap.on('click', logCurrentCoordinates)
 
 var latlngs = [
-  [51.499233, -0.087204],
-  [51.508529, -.081539]
+  [30.4117, -91.18425],
+  [30.3892, -91.0879]
 ];
-var polyline = L.polyline (latlngs, {color: 'red'}).addTo(mymap);
+var polyline = L.polyline (latlngs, {color: 'purple'}).addTo(mymap);
 map.fitBounds(polyline.getBounds());
