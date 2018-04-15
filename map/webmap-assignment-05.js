d9485bc6917494a13635f858d1085bd42923c228
dbell18 <35709782+dbell18@users.noreply.github.com>
2018-04-14 19:56:26 -0500

    Map 5

--- map/webmap-assignment-05.js
+++ map/webmap-assignment-05.js
@@ -1,33 +1,59 @@
 let mymap = L.map('mappicture').setView([39, -98], 4)
 let topography = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}').addTo(mymap)
-let night = L.tileLayer('https://map1.vis.earthdata.nasa.gov/wmts-webmerc/VIIRS_CityLights_2012/default/{time}/{tilematrixset}{maxZoom}/{z}/{y}/{x}.{format}')
+let night = L.tileLayer('https://map1.vis.earthdata.nasa.gov/wmts-webmerc/VIIRS_CityLights_2012/default/{time}/{tilematrixset}{maxZoom}/{z}/{y}/{x}.{format}', {
+	attribution: 'Imagery provided by services from the Global Imagery Browse Services (GIBS), operated by the NASA/GSFC/Earth Science Data and Information System (<a href="https://earthdata.nasa.gov">ESDIS</a>) with funding provided by NASA/HQ.',
+	bounds: [[-85.0511287776, -179.999999975], [85.0511287776, 179.999999975]],
+	minZoom: 1,
+	maxZoom: 8,
+	format: 'jpg',
+	time: '',
+	tilematrixset: 'GoogleMapsCompatible_Level'
+});
 let nature = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}')
 
-let myBasemaps = {
+let diffMaps = {
   'Day map': topography,
   'Night map': night,
   'Nature map': nature
 }
 
->.control.layers(myBasemaps).addTo(mymap)
+L.control.layers(diffMaps).addTo(mymap)
 //in the inndex script src = "stateDemographics.js"
-let stateStyle = function (state){
-  let age = state.properties.MED_AGE
-  let stateColor= 'Olive'
-  if (age < 38) {color = 'green'}
-let formatting = {
-color: 'orange',
-weight: 10
+function mappicture () {
+mymap.on('click', function (event) {
+  console.log('You clicked the map at ' + event.latlng)
+})
+//new name
+//let stateDemographics = {Luke's coords or not (put it in another file by right clicking map folder, new file
+//.js in the new file you can do the let = whatever and paste in the Geojson)}
+function pageDecor (feature) {
+  let male = feature.properties.MALES
+  let female = feature.properties.FEMALES
+  let color = '#89cff0'
+  if (male < female) {
+    color = '#F4C2C2'
+  }
+  let pageDecor = {
+    color: color,
+    opacity: 1.3,
+    fillOpacity: .4,
+    weight: 1.5,
+    stroke: true
+  }
+  return pageDecor
+  }
+  function myPopup (feature, layer) {
+    let name = feature.properties.STATE_NAME
+    let pop = feature.properties.POPULATION
+    let male = feature.properties.MALES
+    let female = feature.properties.FEMALES
+    layer.bindPopup('Population of ' + name + ':' + pop + '<br>National Median of Population: 6174910' + '<br>Number of males in ' + name + ':' + male + '<br>Number of females in ' + name + ':' + female)
+
+  }
+  let myOptions = {
+    style: pageDecor,
+    onEachFeature: myPopup
+  }
+  L.geoJSON(coordinates, myOptions).addTo(mymap)
 }
-return formatting
-}
-let creataePopup = function (state, layer) {
-  let name = feature.properties.STATE_NAME
- let age = feature.properties.MED_AGE
- layer.bindPopup('Median age of ' + name + ': ' + age + '<br>National average: 38')
-}
-let StateOptions = {
-  style: stateStyle,
-  onEachFeature
-}
-L.geoJson(stateCoordinates, stateOptions).addTo(mymap)//new name
+mappicture()
